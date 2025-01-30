2725fb4b0008be1b368b9d38750171ed5a37d910
Nikola Nikolovski <nikolovski.nikola42@gmail.com>
2025-01-30 13:02:29 +0100

    Refactor sidebar components: Remove models API and move chat titles logic
    - Removed the api part of the models sidebar.
    - Moved logic for getting chat titles in history-sidebar component. Before it was in the chat component.

--- src/app/features/chat/services/chat.service.ts
+++ src/app/features/chat/services/chat.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError, map, Observable, throwError} from 'rxjs';
 import {Chat, ChatApi, ChatModel} from '../models/chat';
@@ -13,19 +13,13 @@ export class ChatService {
     ? `${environment.protocol}://${environment.apiUrl}:${environment.port}/chat/`
     : `${environment.protocol}://${environment.apiUrl}/chat/`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  getChats(): Observable<Map<string,Chat[]>> {
+  getChats(): Observable<Map<string, Chat[]>> {
     return this.http.get<any>(`${this.baseUrl}get_chats/`)
   }
 
-  add_chat(user_messages: string[], assistant_messages: string[]): Observable<any> {
-    return this.http.post(`${this.baseUrl}add_chat/`, { "user_messages":user_messages, "assistant_messages":assistant_messages }, {});
-  }
-
-  update_chat(chat_id: string, user_messages: string[], assistant_messages: string[]): Observable<any> {
-    return this.http.post(`${this.baseUrl}update_chat/${chat_id}`, { "user_messages":user_messages, "assistant_messages":assistant_messages }, {});
-  }
 
   get_chat_messages(chat_id: string): Observable<any> {
     return this.http.get(`${this.baseUrl}get_chat_messages/${chat_id}`);
@@ -34,21 +28,21 @@ export class ChatService {
   getChatApiAndModels(type: string): Observable<{ models: ChatModel[]; api: ChatApi }> {
     const url = `${this.baseUrl}get_chat_api_and_models/`; // Adjust the endpoint as needed
     return this.http
-      .get<{ models: ChatModel[]; api: ChatApi }>(url, { params: { type } })
+      .get<{ models: ChatModel[]; api: ChatApi }>(url, {params: {type}})
       .pipe(
         catchError(this.handleError)
       );
   }
 
-  addChatModel(name: string, chatApiType: string): Observable<any>{
-    return this.http.post(`${this.baseUrl}add_chat_model/`, { "name":name, "chat_api_type":chatApiType }, {});
+  addChatModel(name: string, chatApiType: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}add_chat_model/`, {"name": name, "chat_api_type": chatApiType}, {});
   }
 
-  setActiveChatModel(model: string, type: string): Observable<any>{
-    return this.http.post(`${this.baseUrl}set_active_model/`, { "model":model, "type":type }, {});
+  setActiveChatModel(model: string, type: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}set_active_model/`, {"model": model, "type": type}, {});
   }
 
-  getActiveChatModel(): Observable<ChatModel>{
+  getActiveChatModel(): Observable<ChatModel> {
     return this.http.get<ChatModel>(`${this.baseUrl}get_active_model/`);
   }
 
